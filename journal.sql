7bff778e2f756c3ee70e9718eaf094c4be51f06a
Nejer6 <94484058+Nejer6@users.noreply.github.com>
2022-04-06 18:47:07 +0300

    Update journal.sql

--- journal.sql
+++ journal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Мар 30 2022 г., 13:42
+-- Время создания: Апр 06 2022 г., 18:44
 -- Версия сервера: 8.0.24
 -- Версия PHP: 7.1.33
 
@@ -20,6 +20,32 @@ SET time_zone = "+00:00";
 --
 -- База данных: `journal`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `amendments`
+--
+
+CREATE TABLE `amendments` (
+  `id` int NOT NULL,
+  `jourmal_id` int NOT NULL,
+  `meters` int NOT NULL,
+  `amendment` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `journals`
+--
+
+CREATE TABLE `journals` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `note` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -43,6 +69,45 @@ INSERT INTO `roles` (`id`, `role`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `stations`
+--
+
+CREATE TABLE `stations` (
+  `id` int NOT NULL,
+  `jourmal_id` int NOT NULL,
+  `number` int NOT NULL,
+  `level` float NOT NULL,
+  `sighting_point1` int NOT NULL,
+  `sighting_point2` int NOT NULL,
+  `sighting_point3` int NOT NULL,
+  `sighting_point4` int NOT NULL,
+  `horizontal_L_R1` tinyint(1) NOT NULL,
+  `horizontal_L_R2` tinyint(1) NOT NULL,
+  `horizontal_L_R3` tinyint(1) NOT NULL,
+  `horizontal_L_R4` tinyint(1) NOT NULL,
+  `horizontal_degree1` int NOT NULL,
+  `horizontal_minute1` float NOT NULL,
+  `horizontal_degree2` int NOT NULL,
+  `horizontal_minute2` float NOT NULL,
+  `horizontal_degree3` int NOT NULL,
+  `horizontal_minute3` float NOT NULL,
+  `horizontal_degree4` int NOT NULL,
+  `horizontal_minute4` float NOT NULL,
+  `level1` float DEFAULT NULL,
+  `level2` float DEFAULT NULL,
+  `vertical_L_R1` tinyint(1) DEFAULT NULL,
+  `vertical_L_R2` tinyint(1) DEFAULT NULL,
+  `vertical_degree1` int DEFAULT NULL,
+  `vertical_minute1` float DEFAULT NULL,
+  `vertical_degree2` int DEFAULT NULL,
+  `vertical_minute2` float DEFAULT NULL,
+  `by_rail1` float DEFAULT NULL,
+  `by_rail2` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `users`
 --
 
@@ -59,17 +124,38 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `role`, `mail`) VALUES
-(1, 'Admin', 'password', 1, 'Mail@gmail.com');
+(1, 'Admin', 'password', 1, 'Mail@mail.com');
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
+-- Индексы таблицы `amendments`
+--
+ALTER TABLE `amendments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `jourmal_id` (`jourmal_id`);
+
+--
+-- Индексы таблицы `journals`
+--
+ALTER TABLE `journals`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Индексы таблицы `roles`
 --
 ALTER TABLE `roles`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `stations`
+--
+ALTER TABLE `stations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `jourmal_id` (`jourmal_id`);
 
 --
 -- Индексы таблицы `users`
@@ -83,10 +169,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `amendments`
+--
+ALTER TABLE `amendments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `journals`
+--
+ALTER TABLE `journals`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT для таблицы `roles`
 --
 ALTER TABLE `roles`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `stations`
+--
+ALTER TABLE `stations`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
@@ -97,6 +201,24 @@ ALTER TABLE `users`
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
+
+--
+-- Ограничения внешнего ключа таблицы `amendments`
+--
+ALTER TABLE `amendments`
+  ADD CONSTRAINT `amendments_ibfk_1` FOREIGN KEY (`jourmal_id`) REFERENCES `journals` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `journals`
+--
+ALTER TABLE `journals`
+  ADD CONSTRAINT `journals_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `stations`
+--
+ALTER TABLE `stations`
+  ADD CONSTRAINT `stations_ibfk_1` FOREIGN KEY (`jourmal_id`) REFERENCES `journals` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `users`
