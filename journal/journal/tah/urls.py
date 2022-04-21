b09e42d44970c1873425503ea7fb340c520a83ad
KirillTretyakov <kir.2001.tr.sp@mail.ru>
2022-04-21 22:04:24 +0300

    мои изменения

--- journal/journal/tah/urls.py
+++ journal/journal/tah/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
-from journal.tah.views import (index)
+from django.views.generic.base import TemplateView
+from journal.tah.views import (index, teor1)
 
 app_name = "tah"
 urlpatterns = [
-    path("", view=index, name="index"),
+    path("блаблабла/", view=index, name="index"),
+    path('teor1/',view=teor1, name="teor1"),
 ]
--- journal/journal/tah/views.py
+++ journal/journal/tah/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -8,3 +9,10 @@ def index(request):
         'data': "SomeBody Tolds me"
         }
     return render(request, 'tah/index.html', context)
+
+@login_required
+def teor1(request):
+    return render(request, 'teor/teor1.html')
+
+
+
